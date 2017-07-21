ba8379fd7565e4bcf8315fd1d19a0a5570e7ece0
Arthur Landim <landim.arthur@gmail.com>
2017-07-21 10:20:05 -0300

    Logging level example

---
+++ LoggingLevel/LoggingLevel/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LoggingLevel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LoggingLevel")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9054CAD2-8F17-4C92-8BAB-B104497A2C2A")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LoggingLevel/LoggingLevelConsole/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Cassandra;
+
+namespace LoggingLevelConsole
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+            Using ccm (https://github.com/pcmanus/ccm), create a 3 node cluster, start it and pause 1 node
+            > ccm create test -n 3 -v 3.10
+            > ccm start
+            > ccm node3 stop
+            */
+            try
+            {
+                SimpleLoggingLevelTest();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        
+        public static void SimpleLoggingLevelTest()
+        {
+            /*
+            If change the log level to Error the console message:
+            Host: 07/21/2017 10:16:40.885 -03:00 #WARNING: Host 127.0.0.3:9042 considered as DOWN.
+            will not be shown
+            */
+            Diagnostics.CassandraTraceSwitch.Level = TraceLevel.Warning;
+
+            // Add a standard .NET trace listener
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+            
+            const string ip = "127.0.0.1";
+            var queryOptions = new QueryOptions();
+            queryOptions.SetConsistencyLevel(ConsistencyLevel.All);
+            using (var cluster = Cluster.Builder()
+                .AddContactPoints(ip)
+                .WithQueryOptions(queryOptions)
+                .Build())
+            {
+                var cassandraSession = cluster.Connect();
+                var rs = cassandraSession.Execute("select * from system.peers;");
+                foreach (var row in rs)
+                {
+                    Console.WriteLine("{0} {1}", row["peer"], String.Join(",", (string[]) row["tokens"]));
+                }
+            }
+        }
+    }
+}
